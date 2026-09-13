a09fea99756f6bd9ddccf2b8216baf45dbdf6f5c
coderTtxi12 <oliver.coding8@gmail.com>
2026-09-13 04:26:49 -0600

    feat(rider): centralize public rider legal URLs
    - Point in-app legal links at mxy.mx/rider terms and privacy

---
+++ apps/rider/lib/legal/rider_legal_urls.dart
@@ -0,0 +1,7 @@
+/// Public legal URLs for Mexy Rider (also used for Play Store / App Store).
+class RiderLegalUrls {
+  RiderLegalUrls._();
+
+  static const String terms = 'https://mxy.mx/rider/terminos';
+  static const String privacy = 'https://mxy.mx/rider/privacidad';
+}
